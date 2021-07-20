d18eeee5165c8cccd872eb139f0d6b006e315260
Angela Aciobanitei <aciobanitei.angela@gmail.com>
2021-07-20 18:19:57 +0100

    Add test for breeds list screen

--- app/src/androidTest/java/com/ang/acb/dogbreeds/HiltExt.kt
+++ app/src/androidTest/java/com/ang/acb/dogbreeds/HiltExt.kt
@@ -3,7 +3,6 @@ package com.ang.acb.dogbreeds
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
-import androidx.annotation.StyleRes
 import androidx.core.util.Preconditions
 import androidx.fragment.app.Fragment
 import androidx.test.core.app.ActivityScenario
@@ -21,7 +20,6 @@ import androidx.test.core.app.ApplicationProvider
  */
 inline fun <reified T : Fragment> launchFragmentInHiltContainer(
     fragmentArgs: Bundle? = null,
-    @StyleRes themeResId: Int = R.style.FragmentScenarioEmptyFragmentActivityTheme,
     crossinline action: Fragment.() -> Unit = {}
 ) {
     val startActivityIntent = Intent.makeMainActivity(
@@ -29,9 +27,6 @@ inline fun <reified T : Fragment> launchFragmentInHiltContainer(
             ApplicationProvider.getApplicationContext(),
             HiltTestActivity::class.java
         )
-    ).putExtra(
-        "androidx.fragment.app.testing.FragmentScenario.EmptyFragmentActivity.THEME_EXTRAS_BUNDLE_KEY",
-        themeResId
     )
 
     ActivityScenario.launch<HiltTestActivity>(startActivityIntent).onActivity { activity ->
--- app/src/androidTest/java/com/ang/acb/dogbreeds/list/BreedsListFragmentTest.kt
+++ app/src/androidTest/java/com/ang/acb/dogbreeds/list/BreedsListFragmentTest.kt
@@ -1,12 +1,17 @@
 package com.ang.acb.dogbreeds.list
 
+import android.content.Context
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.ang.acb.dogbreeds.FakeBreedsRepository
@@ -15,10 +20,14 @@ import com.ang.acb.dogbreeds.R
 import com.ang.acb.dogbreeds.domain.Breed
 import com.ang.acb.dogbreeds.domain.BreedsGateway
 import com.ang.acb.dogbreeds.domain.SubBreed
+import com.ang.acb.dogbreeds.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -68,6 +77,66 @@ class BreedsListFragmentTest {
         onView(withText(testBreedsList[3].name.capitalize())).check(matches(isDisplayed()))
     }
 
+    @Test
+    fun loadDogBreedsList_error_loadingStops() {
+        // Given 2 breeds that are saved in the repo, make the repository return errors
+        val testBreedsList = listOf(
+            Breed("collie", listOf(SubBreed("border"))),
+            Breed("corgi", listOf(SubBreed("cardigan"))),
+        )
+        runBlocking {
+            (repository as FakeBreedsRepository).addBreeds(testBreedsList)
+            (repository as FakeBreedsRepository).setReturnError(true)
+
+        }
+        // When on start
+        launchActivity()
+
+        // Then the progress bar is hidden
+        onView(withId(R.id.progressBar)).check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun loadDogBreedsList_error_snackBarErrorMessageIsShown() {
+        // Given 2 breeds that are saved in the repo, make the repository return errors
+        val testBreedsList = listOf(
+            Breed("collie", listOf(SubBreed("border"))),
+            Breed("corgi", listOf(SubBreed("cardigan"))),
+        )
+        runBlocking {
+            (repository as FakeBreedsRepository).addBreeds(testBreedsList)
+            (repository as FakeBreedsRepository).setReturnError(true)
+
+        }
+        // When on start
+        launchActivity()
+
+        // Then an error message is shown
+        val message =
+            getApplicationContext<Context>().getString(R.string.get_breeds_list_error_message)
+        onView(withText(message)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun clickOnBreedItem_navigatesToBreedImagesFragment() {
+        // Given 2 breeds that are saved in the repo
+        val testBreedsList = listOf(
+            Breed("collie", listOf(SubBreed("border"))),
+            Breed("corgi", listOf(SubBreed("cardigan"))),
+        )
+        runBlocking {
+            (repository as FakeBreedsRepository).addBreeds(testBreedsList)
+        }
+        // When on the home screen, clicking on the first item
+        val navController = TestNavHostController(getApplicationContext())
+        launchBreedsListFragment(navController)
+
+        onView(withText(testBreedsList[0].name.capitalize())).perform(ViewActions.click())
+
+        // Then we navigate to the breed images screen
+        assertThat(navController.currentDestination?.id, `is`(R.id.breedImagesFragment))
+    }
+
     private fun launchActivity(): ActivityScenario<MainActivity>? {
         val activityScenario = launch(MainActivity::class.java)
         activityScenario.onActivity { activity ->
@@ -75,5 +144,13 @@ class BreedsListFragmentTest {
             (activity.findViewById(R.id.rvBreeds) as RecyclerView).itemAnimator = null
         }
         return activityScenario
+    }
+
+    private fun launchBreedsListFragment(navController: TestNavHostController) {
+        launchFragmentInHiltContainer<BreedsListFragment>(Bundle()) {
+            navController.setGraph(R.navigation.main_navigation)
+            navController.setCurrentDestination(R.id.breedsListFragment)
+            Navigation.setViewNavController(requireView(), navController)
+        }
     }
 }
